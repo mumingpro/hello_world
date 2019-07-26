3f976ba6bc77c19d674e5d75762213858b5ac040
muming <muming@jiaxiaowei.com>
2019-07-26 17:17:34 +0800

    创建了一个hello_world项目并提交了全部文件

---
+++ hello_world/src/com/jiaxiaowei/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.jiaxiaowei;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+        System.out.println("helloWorld!!");
+    }
+}
